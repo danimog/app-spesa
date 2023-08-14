7355c23a1712fe801a75a8da501f537b0f665002
Daniele DM. Moggia <danielemoggia@INFORMATICA3.csestri.local>
2023-08-14 12:14:24 +0200

    add table

--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -1,6 +1,28 @@
 <template>
-    <div>
-{{ res }}
+    <div class="container">
+        <!-- {{ res }} -->
+        <table class="table table-responsive table-success table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Prodotto</th>
+                    <th scope="col">Quantita</th>
+                    <th scope="col">Note</th>
+                    <th scope="col">Data Creazione</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="r in res.documents">
+                    <td> {{ r.$id }} </td>
+                    <td>{{ r.categoria }}</td>
+                    <td>{{ r.prodotto }}</td>
+                    <td>{{ r.quantita }}</td>
+                    <td>{{ r.note }}</td>
+                    <td>{{ r.$createdAt }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -14,10 +36,10 @@ export default {
 
         const res = ref({});
 
-           
+
         onMounted(async () => {
             let data = await api.listDocuments(Server.collectionID);
-                res.value = data;
+            res.value = data;
         })
 
         return {
